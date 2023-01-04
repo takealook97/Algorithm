af30e5efc648c940ad71820c8700bbd5814b3b76
takealook <takealook97@naver.com>
2023-01-04 16:41:49 +0900

    1052 이진법 풀이로 변경 후 README 보강

--- Main1052.java
+++ Main1052.java
@@ -5,21 +5,22 @@ public class Main1052 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        if (N <= K) {
+        if (N <= K) {//N이 K보다 작을 경우엔 물병을 더 구매하지 않아도된다
             System.out.println(0);
             return;
         }
         int result = 0;
         while (true) {
-            int num = N;
+            char[] bin = Integer.toBinaryString(N).toCharArray();
             int count = 0;
-            while (num != 0) {
-                if (num % 2 != 0) {
+            for (int i = 0; i < bin.length; i++) {
+                if (bin[i] == '1') {
                     count++;
                 }
-                num /= 2;
             }
-            if (count <= K) break;
+            if (count <= K) {
+                break;
+            }
             N++;
             result++;
         }
